671c190f7c1fba3b51817c32a4f639da4ff00c3e
Sunny Shah <sunnymshah95@gmail.com>
2020-05-09 00:12:13 +0530

    added documentation

--- Baseline Model/rnn.py
+++ Baseline Model/rnn.py
@@ -6,7 +6,23 @@ import data_processing
 import matplotlib.pyplot as plt 
 
 class RNN_model:
-    def __init__(self, data, frames, input_size):
+    def __init__(self, frames, input_size):
+        '''
+        set up the RNN model as outlined in Bantupalli and Xie (2018)
+        ---------------------
+        input to the RNN is a matrix of size (num_videos, max_frames, 2048)
+            2048 is the length of the output from the CNN
+
+        Layers in the RNN:
+        ------------------
+        LSTM -> LSTM -> LSTM -> Dense
+
+        Other components of the model:
+        -------------------
+        optimizer = Adam
+        loss function = categorical cross entropy
+        metrics = accuracy
+        '''
         inputs = keras.Input(shape = [None, frames, input_size])
         model = layers.LSTM(64, dropout = 0.2, return_sequences = True)(inputs)
         model = layers.LSTM(64, dropout = 0.2, return_sequences = True)(model)
@@ -23,12 +39,18 @@ class RNN_model:
         '''
         return self.model
 
-def train_test_RNN(data):
-    x_train, y_train, x_test, y_test = data.get_data_for_RNN()
+def train_test_RNN(xtr_path, ytr_path, xts_path, yts_path):
+    '''
+    we obain the train and test data, set up the RNN 
+    we then train the RNN on the train data and evaluate it on the test data
+    we then plot the accuracies and losses for the train/test data over epoch
+    '''
+    x_train = np.load(xtr_path)
+    y_train = np.load(ytr_path)
+    x_test = np.load(xts_path)
+    y_test = np.load(yts_path)
 
-    # print(x_tr.shape, x_ts.shape)
-
-    rnn = RNN_model(data, data.get_max_frames(), 2048)
+    rnn = RNN_model(x_train.shape[2], 2048)
     model = rnn.get_model()
 
     history = model.fit(x_train, y_train, validation_split = 0.16, epochs = 10, batch_size = 32)
@@ -51,3 +73,5 @@ def train_test_RNN(data):
     plt.ylabel('Loss')
     plt.legend(['train, validation'])
     plt.show()
+
+
