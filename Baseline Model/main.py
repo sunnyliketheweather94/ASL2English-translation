19b72363190a6eb531df05079602abc970ff72c4
Sunny Shah <sunnymshah95@gmail.com>
2020-05-09 03:21:16 +0530

    Update main.py

--- Baseline Model/main.py
+++ Baseline Model/main.py
@@ -7,10 +7,10 @@ import cnn
 import rnn
 import data_processing
 
-# labels_path = os.path.join('../../Data', 'labels.csv') # CHANGE THIS
-# data_path = '../../Data/Processed Data/' # CHANGE THIS
+labels_path = os.path.join('../../Data', 'labels.csv') # CHANGE THIS
+data_path = '../../Data/Processed Data/' # CHANGE THIS
 
-# data = data_processing.Dataset(data_path, labels_path)
-# x_train, y_train, x_test, y_test = data.get_data_for_RNN()
+data = data_processing.Dataset(data_path, labels_path)
+x_train, y_train, x_test, y_test = data.get_data_for_RNN()
 
-rnn.train_test_RNN('x_train.npy', 'y_train.npy', 'x_test.npy', 'y_test.npy')
+# rnn.train_test_RNN('x_train.npy', 'y_train.npy', 'x_test.npy', 'y_test.npy')
