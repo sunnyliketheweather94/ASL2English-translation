80c2dd4f38ac0c0d56720630e65402d666dab488
Sunny Shah <sunnymshah95@gmail.com>
2020-05-08 21:53:09 +0530

    Update main.py

--- Baseline Model/main.py
+++ Baseline Model/main.py
@@ -7,26 +7,19 @@ import cnn
 import rnn
 import data_processing
 
-
-
 labels_path = os.path.join('../../Data', 'labels.csv') ##CHANGE THIS
 data_path = '../../Data/Processed Data/' #CHANGE THIS
 
 data = data_processing.Dataset(data_path, labels_path)
-train = data.get_testData()
+test = data.get_testData()
 
 model = cnn.Inception_Model()
 
-# paths = train['frame_paths']
-video_num = train['Video']
+# paths = test['frame_paths']
 
-num = train['Video'][0]
+# f = model.extract_features(paths[0])
+# print(f)
+# print(len(f))
 
-paths = data.get_frame_paths(num)
-
-f = model.extract_features(paths[0])
-print(f)
-print(len(f))
-
-x_train, y_train, x_dev, y_dev, x_test, y_test = data_processing.get_data_for_RNN()
-rnn.train_test_RNN(data, x_train, y_train, x_test, y_test)
+x_train, y_train, x_test, y_test = data.get_data_for_RNN()
+# rnn.train_test_RNN(data)
