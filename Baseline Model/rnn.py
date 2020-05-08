1ede0ffb4f898ba27ca53004f9ed0df972c84138
Sunny Shah <sunnymshah95@gmail.com>
2020-05-08 23:49:04 +0530

    updated rnn.py according to the input data

--- Baseline Model/rnn.py
+++ Baseline Model/rnn.py
@@ -3,14 +3,15 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, models
 import data_processing
+import matplotlib.pyplot as plt 
 
 class RNN_model:
-    def __init__(self, data):
+    def __init__(self, data, frames, input_size):
         inputs = keras.Input(shape = [None, frames, input_size])
-        model = tf.keras.layers.LSTM(64, dropout = 0.2, return_sequences = True)(inputs)
-        model = tf.keras.layers.LSTM(64, dropout = 0.2, return_sequences = True)(model)
-        model = tf.keras.layers.LSTM(64, dropout = 0.2, return_sequences = True)(model)
-        model = tf.keras.Dense(data.get_num_classes(), activation = 'softmax')
+        model = layers.LSTM(64, dropout = 0.2, return_sequences = True)(inputs)
+        model = layers.LSTM(64, dropout = 0.2, return_sequences = True)(model)
+        model = layers.LSTM(64, dropout = 0.2, return_sequences = True)(model)
+        model = layers.Dense(data.get_num_classes(), activation = 'softmax')
         model.compile(optimizer = 'Adam', loss = "categorical_cross_entropy", metrics = ['accuracy'])
         self.model = model
 
@@ -22,8 +23,31 @@ class RNN_model:
         '''
         return self.model
 
-def train_test_RNN(data, x_train, y_train, x_test, y_test):
-    rnn = RNN_model(data)
+def train_test_RNN(data):
+    x_train, y_train, x_test, y_test = data.get_data_for_RNN()
+
+    # print(x_tr.shape, x_ts.shape)
+
+    rnn = RNN_model(data, data.get_max_frames(), 2048)
     model = rnn.get_model()
-    model.fit(x_train, y_train, epochs = 10, batch_size = 32)
+
+    history = model.fit(x_train, y_train, validation_split = 0.16, epochs = 10, batch_size = 32)
     test_loss, acc = model.evaluate(x_test, y_test)
+
+    # plot the accuracy over epochs
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Model accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend(['train', 'validation'])
+    plt.show()
+
+    # plot the losses
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend(['train, validation'])
+    plt.show()
