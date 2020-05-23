2266d74c8f5c23cd212afc342bf69c1ed85f87df
Sunny Shah <sunnymshah95@gmail.com>
2020-05-23 06:14:34 +0530

    Update main.py

--- Baseline Model/main.py
+++ Baseline Model/main.py
@@ -3,17 +3,27 @@ import matplotlib.pyplot as pyplot
 import os
 import cv2
 
-#import cnn
-#import rnn
-#import data_processing
+import cnn
+import rnn
+import data_processing
 
+# for teresa's local computer
 #labels_path = os.path.join('/Users/teresanoyola/Desktop/CS230/Project/ASL-English-translation/Baseline Model', 'labels.csv') # CHANGE THIS
 #data_path = '/Users/teresanoyola/Desktop/CS230/Project/Processed Data/' # CHANGE THIS
-#Google Drive
-labels_path = '/content/drive/My Drive/CS230/Data/labels.csv'
-data_path = '/content/drive/My Drive/CS230/Data/Processed Data'
 
-data = Dataset(data_path, labels_path)
+# for sunny's local computer
+labels_path = os.path.join('../../Data', 'labels.csv') # CHANGE THIS
+data_path = '../../Data/Processed Data/' # CHANGE THIS
+
+# for georgia
+
+
+
+# Teresa's Google Drive
+# labels_path = '/content/drive/My Drive/CS230/Data/labels.csv'
+# data_path = '/content/drive/My Drive/CS230/Data/Processed Data'
+
+data = data_processing.Dataset(data_path, labels_path)
 #print(data.get_max_frames())
 data.get_data_for_CNN()
 
