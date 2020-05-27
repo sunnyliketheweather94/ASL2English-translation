3fb9aee70fed81197842e8d2adfc269285ed163a
Sunny Shah <sunnymshah95@gmail.com>
2020-05-27 09:40:26 +0530

    Made final changes

--- cnn.py
+++ cnn.py
@@ -37,12 +37,20 @@ class ConvNet:
         # "regular"
         model = models.Sequential()
 
-        model.add(Conv3D(32, kernel_size = (5, 5, 5), input_shape = self.shape))
+        model.add(Conv3D(16, kernel_size = (5, 5, 5), input_shape = self.shape))
         model.add(Activation('relu'))
+        model.add(BatchNormalization())
         model.add(MaxPooling3D(pool_size = (5, 5, 5)))
 
-        model.add(Conv3D(64, kernel_size = (5, 5, 5)))
+        model.add(Conv3D(32, kernel_size = (5, 5, 5)))
         model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling3D(pool_size = (5, 5, 5)))
+
+        model.add(Conv3D(32, kernel_size = (5, 5, 5)))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling3D(pool_size = (5, 5, 5)))
 
         model.add(Flatten())
         model.add(Dense(512, activation = 'sigmoid'))
@@ -54,19 +62,38 @@ class ConvNet:
         # deeper
         model = models.Sequential()
 
-        model.add(Conv3D(32, kernel_size = (5, 5, 5), input_shape = self.shape))
+        model.add(Conv3D(16, kernel_size = (5, 5, 5), input_shape = self.shape))
         model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling3D(pool_size = (5, 5, 5)))
+
+        model.add(Conv3D(16, kernel_size = (5, 5, 5)))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling3D(pool_size = (5, 5, 5)))
+
+        model.add(Conv3D(32, kernel_size = (5, 5, 5)))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling3D(pool_size = (5, 5, 5)))
+
+        model.add(Conv3D(32, kernel_size = (5, 5, 5)))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
         model.add(MaxPooling3D(pool_size = (5, 5, 5)))
 
         model.add(Conv3D(48, kernel_size = (5, 5, 5)))
         model.add(Activation('relu'))
+        model.add(BatchNormalization())
         model.add(MaxPooling3D(pool_size = (5, 5, 5)))
 
         model.add(Conv3D(64, kernel_size = (5, 5, 5)))
         model.add(Activation('relu'))
+        model.add(BatchNormalization())
         model.add(MaxPooling3D(pool_size = (5, 5, 5)))
 
         model.add(Flatten())
+        model.add(Dense(1024, activation = 'sigmoid'))
         model.add(Dense(512, activation = 'sigmoid'))
         model.add(Dense(self.num_classes, activation = 'softmax'))
 
@@ -76,12 +103,14 @@ class ConvNet:
         # wider
         model = models.Sequential()
 
-        model.add(Conv3D(64, kernel_size = (5, 5, 5), input_shape = self.shape))
+        model.add(Conv3D(16, kernel_size = (5, 5, 5), input_shape = self.shape))
         model.add(Activation('relu'))
+        model.add(BatchNormalization())
         model.add(MaxPooling3D(pool_size = (5, 5, 5)))
 
         model.add(Conv3D(128, kernel_size = (5, 5, 5)))
         model.add(Activation('relu'))
+        model.add(BatchNormalization())
         model.add(MaxPooling3D(pool_size = (5, 5, 5)))
 
         model.add(Flatten())
@@ -100,26 +129,33 @@ class ConvNet:
         x_test = np.load(xts_path)
         y_test = np.load(yts_path)
 
-        self.model.compile(optimizer = 'Adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
-        self.history = self.model.fit(x_train, y_train, epochs = num_epochs, batch_size = batch)
-        test_eval = self.model.evaluate(x_test, y_test)
-
-        print("Test evaluation:\nLoss = {}\nCategorical Accuracy = {}".format(x[0], x[1]))
+        opt = optimizers.Adam(learning_rate = 0.00001, beta_1 = 0.95, beta_2 = 0.98)
+        # opt = optimizers.SGD(learning_rate = 0.0001)
+        self.model.compile(optimizer = opt, loss = 'categorical_crossentropy', metrics = ['accuracy'])
+        self.history = self.model.fit(x_train, y_train.T, epochs = num_epochs, batch_size = batch)
+        
+        # test_eval = self.model.evaluate(x_test, y_test.T)
+        # print("Test evaluation:\nLoss = {}\nAccuracy = {}".format(test_eval[0], test_eval[1]))
 
         self.plot_accuracy()
         self.plot_loss()
 
-    def plot_accuracy(self):
+    def plot_accuracy(self, save = True):
         hist = self.history 
 
         # plot the accuracy over epochs
-        plt.plot(hist.history['categorical_accuracy'])
-        plt.title('Model categorical accuracy')
+        plt.plot(hist.history['accuracy'])
+        plt.title('Model accuracy')
         plt.xlabel('Epoch')
         plt.ylabel('Accuracy')
-        plt.show()
+        
+        if not save:
+            plt.show()
 
-    def plot_loss(self):
+        else:
+            plt.savefig('accuracy.png')
+
+    def plot_loss(self, save = True):
         hist = self.history 
 
         # plot the loss over epochs
@@ -127,4 +163,9 @@ class ConvNet:
         plt.title('Model loss')
         plt.xlabel('Epoch')
         plt.ylabel('Loss')
-        plt.show()
+        
+        if not save:
+            plt.show()
+
+        else:
+            plt.savefig('loss.png')
