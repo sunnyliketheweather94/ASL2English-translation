0970e27a739b97d9af2e56bab961e261346ce8e2
Sunny Shah <sunnymshah95@gmail.com>
2020-05-27 06:24:23 +0530

    Added new models

--- 3D CNN/cnn.py
+++ 3D CNN/cnn.py
@@ -10,32 +10,20 @@ from tensorflow.keras import optimizers, losses
 from tensorflow.keras import models
 
 class ConvNet:
-    def __init__(self, shape, num_classes):
-        model = models.Sequential()
+    def __init__(self, shape, num_classes, model_num):
+        self.img_shape = shape
+        self.num_classes = num_classes
 
-        # frames, height, width, channels = shape
-        # shape = (frames, height, width, channels)
+        if model_num == 1:
+            model = self.get_model1()
 
-        model.add(Conv3D(32, kernel_size = (3, 3, 3), input_shape = shape))
-        model.add(Activation('relu'))
+        elif model_num == 2:
+            model = self.get_model2()
 
-        # model.add(Conv3D(32, kernel_size = (3, 3, 3)))
-        # model.add(Activation('relu'))
-        model.add(MaxPooling3D(pool_size = (3, 3, 3)))
+        elif model_num == 3:
+            model = self.get_model3()
 
-        model.add(Conv3D(64, kernel_size = (3, 3, 3)))
-        model.add(Activation('relu'))
-        # model.add(Dropout(0.20))
 
-        # model.add(Conv3D(64, kernel_size = (3, 3, 3)))
-        # model.add(Activation('relu'))
-
-        model.add(Flatten())
-        model.add(Dense(1024, activation = 'sigmoid'))
-        # model.add(Dense(512, activation = 'sigmoid'))
-        model.add(Dense(num_classes, activation = 'softmax'))
-
-        model.compile(optimizer = 'Adam', loss = 'categorical_crossentropy', metrics = ['categorical_accuracy'])
         self.model = model
 
 
@@ -45,7 +33,64 @@ class ConvNet:
     def get_model(self):
         return self.model
 
-    def train(self, train_paths, test_paths, num_epochs = 15, batch = 64):
+    def get_model1(self):
+        # "regular"
+        model = models.Sequential()
+
+        model.add(Conv3D(32, kernel_size = (5, 5, 5), input_shape = self.shape))
+        model.add(Activation('relu'))
+        model.add(MaxPooling3D(pool_size = (5, 5, 5)))
+
+        model.add(Conv3D(64, kernel_size = (5, 5, 5)))
+        model.add(Activation('relu'))
+
+        model.add(Flatten())
+        model.add(Dense(512, activation = 'sigmoid'))
+        model.add(Dense(self.num_classes, activation = 'softmax'))
+
+        return model
+
+    def get_model2(self):
+        # deeper
+        model = models.Sequential()
+
+        model.add(Conv3D(32, kernel_size = (5, 5, 5), input_shape = self.shape))
+        model.add(Activation('relu'))
+        model.add(MaxPooling3D(pool_size = (5, 5, 5)))
+
+        model.add(Conv3D(48, kernel_size = (5, 5, 5)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling3D(pool_size = (5, 5, 5)))
+
+        model.add(Conv3D(64, kernel_size = (5, 5, 5)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling3D(pool_size = (5, 5, 5)))
+
+        model.add(Flatten())
+        model.add(Dense(512, activation = 'sigmoid'))
+        model.add(Dense(self.num_classes, activation = 'softmax'))
+
+        return model
+
+    def get_model3(self):
+        # wider
+        model = models.Sequential()
+
+        model.add(Conv3D(64, kernel_size = (5, 5, 5), input_shape = self.shape))
+        model.add(Activation('relu'))
+        model.add(MaxPooling3D(pool_size = (5, 5, 5)))
+
+        model.add(Conv3D(128, kernel_size = (5, 5, 5)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling3D(pool_size = (5, 5, 5)))
+
+        model.add(Flatten())
+        model.add(Dense(512, activation = 'sigmoid'))
+        model.add(Dense(self.num_classes, activation = 'softmax'))
+
+        return model
+
+    def train(self, train_paths, test_paths, num_epochs = 20, batch = 4):
         xtr_path, ytr_path = train_paths
         xts_path, yts_path = test_paths
 
@@ -55,12 +100,11 @@ class ConvNet:
         x_test = np.load(xts_path)
         y_test = np.load(yts_path)
 
-        hist = self.model.fit(x_train, y_train, epochs = num_epochs, batch_size = batch)
+        self.model.compile(optimizer = 'Adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+        self.history = self.model.fit(x_train, y_train, epochs = num_epochs, batch_size = batch)
         test_eval = self.model.evaluate(x_test, y_test)
 
         print("Test evaluation:\nLoss = {}\nCategorical Accuracy = {}".format(x[0], x[1]))
-
-        self.history = hist 
 
         self.plot_accuracy()
         self.plot_loss()
