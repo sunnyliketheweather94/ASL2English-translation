b29f87f0152220d1987d8b599ceb3c63ede2a3e0
Ubuntu <ubuntu@ip-172-31-46-252.us-west-2.compute.internal>
2020-06-10 09:56:46 +0000

    newest

--- cnn.py
+++ cnn.py
@@ -123,13 +123,14 @@ class ConvNet:
     def exp5(self): # dropout + L2-regularization
         model = models.Sequential()
 
-        model.add(Conv3D(16, kernel_size = (3, 3, 3), input_shape = self.img_shape, kernel_regularizer = regularizers.l2(0.1)))
+        model.add(Conv3D(2, kernel_size = (3, 3, 3), input_shape = self.img_shape, kernel_regularizer = regularizers.l2(0.1)))
         model.add(Dropout(0.4))
         model.add(Activation('relu'))
         model.add(BatchNormalization())
         model.add(MaxPooling3D(pool_size = (3, 3, 3), padding = 'valid'))
 
-        model.add(Conv3D(32, kernel_size = (3, 3, 3), kernel_regularizer = regularizers.l2(0.1)))
+        model.add(Conv3D(4, kernel_size = (3, 3, 3)))
+        # model.add(Conv3D(4, kernel_size = (3, 3, 3), kernel_regularizer = regularizers.l2(0.1\ )))    
         model.add(Dropout(0.4))
         model.add(Activation('relu'))
         model.add(BatchNormalization())
